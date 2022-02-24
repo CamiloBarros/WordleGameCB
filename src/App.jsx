ec9e4f55242c1f0c5626f28350a2c40c424a3d17
Camilo <camilobarrosr@gmail.com>
2022-02-24 00:02:26 -0500

    Bug: fix bug of h-screen

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom'
 function App() {
   return(
     <div className='bg-gradient-to-t bg-neutral-50'>
-      <div className='flex flex-col justify-between items-center max-w-md mx-auto h-screen p-4 bg-stone-900'>
+      <div className='flex flex-col justify-between items-center max-w-md mx-auto min-h-screen p-4 bg-stone-900'>
         <Outlet />
       </div>
     </div>
